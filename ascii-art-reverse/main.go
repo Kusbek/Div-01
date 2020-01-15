38a973b34c65f516150736db526c246480a614e6
Bekarys Kuspan <b.kuspan@kazdream.kz>
2020-01-15 19:09:01 +0600

    reverse first commit

--- ascii-art-reverse/main.go
+++ ascii-art-reverse/main.go
@@ -12,18 +12,53 @@ func main() {
 	if e {
 		return
 	}
-	segment(template)
-}
+	segmentedTemplate := segment(template)
+	targetTemplate, e := openFileAndReadLineByLine("file")
+	if e {
+		return
+	}
 
-func segment(template []string) /*[][]string*/ {
-	// result := make([][]string, 126-32)
-
-	for i := 0; i < len(template); i = i + 9 {
-		temp := template[i : i+9]
-		for _, v := range temp {
-			fmt.Println(v)
+	str := ""
+	for len(targetTemplate[0]) > 0 {
+		for i, v := range segmentedTemplate {
+			if checkIfLetterIsPresent(v, targetTemplate) {
+				str = str + string(rune(i+32))
+				targetTemplate = removeLetter(len(v[0]), targetTemplate)
+				fmt.Println(str, len(v[0]))
+				for _, v := range targetTemplate {
+					fmt.Println(v)
+				}
+			}
 		}
 	}
+}
+
+func removeLetter(length int, word []string) []string {
+	for i, v := range word[0 : len(word)-1] {
+		// fmt.Println(v[length:])
+		word[i] = v[length:]
+	}
+	return word
+}
+func checkIfLetterIsPresent(letter, word []string) bool {
+	found := true
+	if len(letter[0]) > len(word[0]) {
+		return false
+	}
+	for i, v := range word[0 : len(word)-1] {
+		if letter[i] != v[:len(letter[i])] {
+			found = false
+		}
+	}
+	return found
+}
+func segment(template []string) [][]string {
+	var result [][]string
+	for i := 0; i < len(template)-1; i = i + 9 {
+		temp := template[i+1 : i+9]
+		result = append(result, temp)
+	}
+	return result
 }
 
 func createArt(text string, template []string) {
