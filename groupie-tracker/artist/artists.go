320be912615843561b9dcf577ec59854b250c345
Bekarys Kuspan <b.kuspan@kazdream.kz>
2020-02-19 11:56:46 +0600

    fas;ldf

--- groupie-tracker/artist/artists.go
+++ groupie-tracker/artist/artists.go
@@ -1,12 +1,18 @@
 package artist
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
 
 type Artists struct {
 	Artists []Artist
 }
 
-func UnmarshalArtist(data []byte) ([]Artist, error) {
+func UnmarshalArtists(data []byte) ([]Artist, error) {
 	var r []Artist
 	err := json.Unmarshal(data, &r)
 	return r, err
@@ -26,4 +32,65 @@ type Artist struct {
 	Locations    string   `json:"locations"`
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
+}
+
+func UnmarshalArtist(data []byte) (Artist, error) {
+	var r Artist
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func HandleArtist(w http.ResponseWriter, req *http.Request) {
+	id := req.URL.Query().Get("id")
+	artist := getArtist(id)
+	tmpl := template.Must(template.ParseFiles("./artist/artist.html"))
+	tmpl.Execute(w, artist)
+}
+
+func getArtist(id string) Artist {
+	url := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/artists/%s", id)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	artist, err := UnmarshalArtist(body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return artist
+}
+func HandleArtists(w http.ResponseWriter, req *http.Request) {
+	artists := getArtists()
+	a := Artists{Artists: artists}
+	tmpl := template.Must(template.ParseFiles("./artist/artists.html"))
+	tmpl.Execute(w, a)
+	// w.Header().Set("Content-Type", "text/html")
+
+	// body, err := artists.Marshal()
+	// // if err != nil {
+	// // 	w.WriteHeader(400)
+	// // 	fmt.Fprintf(w, "Нормально данные вводи, sumelek")
+	// // }
+	// w.Write(body)
+
+}
+
+func getArtists() []Artist {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+	if err != nil {
+		fmt.Println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	artists, err := UnmarshalArtists(body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return artists
 }
--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -1,48 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
-	"io/ioutil"
 	"net/http"
 
-	artist "./artist"
+	"./artist"
 )
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/artists", handleArtists)
+	http.HandleFunc("/artists", artist.HandleArtists)
+	http.HandleFunc("/artist", artist.HandleArtist)
 	http.ListenAndServe(":8080", nil)
-}
-func handleArtists(w http.ResponseWriter, req *http.Request) {
-	artists := getArtists()
-	a := artist.Artists{Artists: artists}
-	tmpl := template.Must(template.ParseFiles("./artist/artists.html"))
-	tmpl.Execute(w, a)
-	// w.Header().Set("Content-Type", "text/html")
-
-	// body, err := artists.Marshal()
-	// // if err != nil {
-	// // 	w.WriteHeader(400)
-	// // 	fmt.Fprintf(w, "Нормально данные вводи, sumelek")
-	// // }
-	// w.Write(body)
-
-}
-
-func getArtists() []artist.Artist {
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
-	if err != nil {
-		fmt.Println(err)
-	}
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	artists, err := artist.UnmarshalArtist(body)
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(artists[0])
-	return artists
 }
