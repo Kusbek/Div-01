4caa584536f75cffc3e38bf401c17bbe6e1046ab
Bekarys Kuspan <kusbek1994@gmail.com>
2020-03-14 18:40:04 +0600

    tipa finished

--- net-cat/server/main.go
+++ net-cat/server/main.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type client struct {
@@ -17,8 +20,9 @@ type client struct {
 }
 
 type message struct {
-	client  *client
-	message string
+	client    *client
+	message   string
+	timestamp string
 }
 
 type chatServer struct {
@@ -28,12 +32,33 @@ type chatServer struct {
 	distr    chan *message
 }
 
+const (
+	LIMIT int = 2
+)
+
 func main() {
-	chat := &chatServer{mutex: &sync.Mutex{}, distr: make(chan *message, 1)}
-	chat.startServer()
+	args := os.Args[1:]
+	var port int
+	var err error
+	if len(args) == 1 {
+		port, err = strconv.Atoi(args[0])
+		if err != nil {
+			fmt.Print("Invalid input:")
+			fmt.Println(err)
+			return
+		}
+	} else if len(args) < 1 {
+		port = 8989
+	} else {
+		fmt.Print("Too many parameters")
+		return
+	}
+
+	chat := &chatServer{mutex: &sync.Mutex{}, distr: make(chan *message, LIMIT)}
+	chat.startServer(port)
 }
-func (self *chatServer) startServer() {
-	addr := "127.0.0.1:8989"
+func (self *chatServer) startServer(port int) {
+	addr := fmt.Sprintf("localhost:%d", port)
 	listener, err := net.Listen("tcp", addr)
 	if err != nil {
 		panic(err)
@@ -46,13 +71,39 @@ func (self *chatServer) startServer() {
 		if err != nil {
 			log.Printf("Error accepting connection from client: %s", err)
 		} else {
-			if len(self.clients) < 2 {
+			if len(self.clients) < LIMIT {
 				client := self.threeWayHandshake(&conn)
 				go client.serve(self)
 				go self.broadcast()
 			} else {
-				fmt.Fprintf(conn, "Sorry, bro, but chat is polnyi$")
+				fmt.Fprintf(conn, "Sorry, bro, but chat is polnyi")
 			}
+		}
+	}
+}
+func (self *client) serve(chat *chatServer) {
+	reader := bufio.NewReader(*self.connection)
+	for {
+		fmt.Fprintf(*self.connection, fmt.Sprintf("[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"), self.name))
+		msg, err := reader.ReadString('\n')
+		if err != nil && err == io.EOF {
+			fmt.Printf("detected closed client: %s\n", self.name)
+			self.remove(chat)
+			chat.goodbye(self)
+			break
+		}
+		time := time.Now().Format("2006-01-02 15:04:05")
+		chat.distr <- &message{client: self, message: msg, timestamp: time}
+		fmt.Printf("[%s][%s]: %s", time, self.name, msg)
+	}
+}
+
+func (self *client) remove(chat *chatServer) {
+	chat.mutex.Lock()
+	defer chat.mutex.Unlock()
+	for i, v := range chat.clients {
+		if v == self {
+			chat.clients = append(chat.clients[:i], chat.clients[i+1:]...)
 		}
 	}
 }
@@ -63,7 +114,8 @@ func (self *chatServer) broadcast() {
 		self.mutex.Lock()
 		for _, client := range self.clients {
 			if client != cl.client {
-				fmt.Fprintf(*client.connection, fmt.Sprintf("%s: %s", cl.client.name, cl.message))
+				fmt.Fprintf(*client.connection, fmt.Sprintf("\n[%s][%s]: %s", cl.timestamp, cl.client.name, cl.message))
+				fmt.Fprintf(*client.connection, fmt.Sprintf("[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"), client.name))
 			}
 		}
 		self.mutex.Unlock()
@@ -72,36 +124,30 @@ func (self *chatServer) broadcast() {
 
 func (self *chatServer) introduce(cl *client) {
 	for _, client := range self.clients {
-		fmt.Fprintf(*client.connection, cl.name+" has joined our chat...\n")
+		fmt.Fprintf(*client.connection, "\n"+cl.name+" has joined our chat...\n")
+		fmt.Fprintf(*client.connection, fmt.Sprintf("[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"), client.name))
+	}
+}
+func (self *chatServer) goodbye(cl *client) {
+	for _, client := range self.clients {
+		fmt.Fprintf(*client.connection, "\n"+cl.name+" has left our chat...\n")
+		fmt.Fprintf(*client.connection, fmt.Sprintf("[%s][%s]:", time.Now().Format("2006-01-02 15:04:05"), client.name))
 	}
 }
 
 func (self *chatServer) threeWayHandshake(conn *net.Conn) *client {
-	fmt.Fprintf(*conn, "Atyndy jaz, sumelek: $")
+	file, _ := os.Open("linux.txt")
+	text, _ := ioutil.ReadAll(file)
+	fmt.Fprintf(*conn, string(text))
+	fmt.Fprintf(*conn, "Atyndy jaz, sumelek: ")
 	reader := bufio.NewReader(*conn)
 	name, _ := reader.ReadString('\n')
 	name = strings.TrimSuffix(name, "\n")
 	client := &client{connection: conn, name: name}
 	self.mutex.Lock()
 	self.introduce(client)
-	fmt.Println("Finished")
 	self.clients = append(self.clients, client)
 	defer self.mutex.Unlock()
 	fmt.Fprintf(os.Stdout, name+" has joined our chat...\n")
 	return client
-}
-
-func (self *client) serve(chat *chatServer) {
-	reader := bufio.NewReader(*self.connection)
-	for {
-		msg, err := reader.ReadString('\n')
-		if err != nil && err == io.EOF {
-			fmt.Printf("detected closed client: %s", self.name)
-			break
-		}
-		chat.mutex.Lock()
-		chat.distr <- &message{client: self, message: msg}
-		chat.mutex.Unlock()
-		fmt.Printf("%s: %s", self.name, msg)
-	}
 }
